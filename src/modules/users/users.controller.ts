f10da141eff655c4d0229862fe027e56f2c453ce
victor <v.prada@udla.edu.co>
2020-07-17 20:55:46 -0500

    feat: add users

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from 'src/entities';
+import { newUserDTO } from './dto/newUser.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private userService : UsersService){}
+
+    @Get()
+    async getUsers() : Promise<User[]>{
+        return await this.userService.getUsers();
+    }
+
+    @Post()
+    async createUser(@Body() data: newUserDTO) : Promise<User>{
+        return await this.userService.createUser(data)
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//solo el controlador get post put delete.
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/entities';
+import { User, Role } from 'src/entities';
 import { Repository } from 'typeorm';
+import { newUserDTO } from './dto/newUser.dto';
 
 @Injectable()
 export class UsersService {
     constructor(@InjectRepository(User) private readonly userRepository: Repository<User>){}
 
-    async getUsers(){
-        return this.userRepository.find()
+    async getUsers() : Promise<User[]>{
+        return await this.userRepository.find({relations: ["role"]});
     }
+
+    //sin dto
+    async createUser(data : newUserDTO): Promise<User>{
+        let user = new User();
+        user.username = data.username;
+        user.role = new Role()
+        user.role.idRole = data.role;
+        user.state = true;
+        return await this.userRepository.save(user);
+      }
+
+    //con dto data transfer object
+      
+      
     
 }
